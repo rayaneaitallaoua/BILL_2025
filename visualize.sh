84b0703ffaac0d7de2eb595ac89495cd0248c542
Rayane Ayoub AIT ALLAOUA <ayoubaitallaoua@gmail.com>
2025-02-05 11:32:36 +0100

    addition to visualize.sh

--- visualize.sh
+++ visualize.sh
@@ -4,6 +4,7 @@
 mkdir -p ./compressed_files
 mkdir -p ./compressed_files/compressed_sv
 mkdir -p ./compressed_files/compressed_snp
+mkdir -p ./combined_vcfs
 mkdir -p ./combined_vcfs/snp
 mkdir -p ./combined_vcfs/sv
 
@@ -22,11 +23,11 @@ for file in *.vcf; do
     tabix -p vcf "$file"
 
     # Move compressed files per variant type
-    if [[ $file == *"snp"*.gz ]]; then
+    if [[ $file == *"snp"*.gz || $file == *"snp"*.tbi ]]; then
 
         mv "$file" ./compressed_files/compressed_snp
 
-    elif [[ $file == *"sv_sniffles"*.gz ]]; then
+    elif [[ $file == *"sv"*.gz || $file == *"sv"*.tbi ]]; then
 
         mv "$file" ./compressed_files/compressed_sv
 
@@ -55,33 +56,18 @@ bcftools merge $to_merge_sv -o ./combined_vcfs/snp/P90_sv_combined.vcf
 
 echo -ne "\rStep 2: Done! \n"
 
-echo "Step 3: Extracting AF Per Gene..."
-for file in ./combined_vcf_per_sample/heat_shock/*.vcf ./combined_vcf_per_sample/cold_shock/*.vcf; do
-    count=$(( (count + 1) % 4 ))
-    echo -ne "\rExtracting: ${SPINNER[$count]}"
+echo "Step 3: Extracting AF Per SV..."
 
-    bcftools query -f '%CHROM\t%POS\t%INFO/ANN\n' "$file" \
-        | cut -d'|' -f4 | uniq -c > ./mutations_per_gene/"$(basename "${file%.vcf}")_mutations_per_gene.txt"
-done
+    bcftools query -f '%INFO/AF\n' ./combined_vcfs/sv/P90_sv_combined.vcf \
+        | cut -d'=' -f1 | uniq -c > "AF_per_SV.txt"
+
 echo -ne "\rStep 3: Done! \n"
 
-echo "Step 4: Counting Mutation Types..."
-for file in ./combined_vcf_per_sample/heat_shock/*.vcf ./combined_vcf_per_sample/cold_shock/*.vcf; do
-    count=$(( (count + 1) % 4 ))
-    echo -ne "\rCounting: ${SPINNER[$count]}"
+echo "Step 4: Extracting GQ Per SNP..."
 
-    bcftools query -f '%REF\t%ALT\t%INFO/SVTYPE\n' "$file" | awk '
-    {
-        if (length($1) == 1 && length($2) == 1) 
-            print "SNP"; 
-        else if ($3 != ".") 
-            print $3; 
-        else if (length($1) < length($2)) 
-            print "Ins"; 
-        else if (length($1) > length($2)) 
-            print "Del";
-    }' | sort | uniq -c | sort -nr > ./mutation_types/"$(basename "${file%.vcf}")_mutation_types.txt"
-done
-echo -ne "\rStep 4: Done! \n"
+    bcftools query -f '%QUAL\n' ./combined_vcfs/snp/P90_snp_combined.vcf \
+        | cut -d'=' -f1 | uniq -c > "AF_per_SNP.txt"
+
+echo -ne "\rStep 3: Done! \n"
 
 echo "All steps completed successfully! "
